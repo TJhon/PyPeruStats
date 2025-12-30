6c2b64b61c6ee0ca42057dedf27c9cb4e69eccdb
Jhon Flores Rojas <48776907+TJhon@users.noreply.github.com>
2025-12-29 23:43:22 -0500

    Remove data fetching logic from _update_data.py

--- _update_data.py
+++ _update_data.py
@@ -1,12 +1,1 @@
-import json
-from pyPeruStatsa.MTC.homologacion import TelMTC
 
-## MTC
-
-homologados_f = "./requests/MTC/homologacion.json"
-with open(homologados_f, "r") as hf:
-    brands = json.load(hf).get("marcas")
-
-for brand in brands:
-    data = TelMTC(brand).fetch_data()
-    print(data)
