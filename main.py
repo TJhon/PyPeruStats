551aff8e0cd20ab695cd2cd3857e9a5270095f5f
tjhon <fr.jhonk@gmail.com>
2026-04-07 16:40:01 -0500

    correccion del tipo de datos

--- main.py
+++ main.py
@@ -14,3 +14,27 @@ print_tree(
 # build: python -m build
 # twine: python -m pip install twine
 # upload twine: twine upload dist/*
+
+from perustats import BCRPDataSeries, BCRPSeries
+
+series = BCRPSeries(
+    [
+        " RD16085DA",
+        "PD04657MD",
+        "PD04646PD",
+        "RD13761DM",
+        "RD13805DM",
+        "      RD13845DM",
+        "RD15478DQ",
+        "RD14266DQ",
+        "CD10401DA",
+        "CD10422DA",
+        "fakecodeA",
+    ],
+    start_date="2000-01-02",
+    end_date="2020-01-01",
+)
+data = BCRPDataSeries(series).fetch_data()
+
+for freq in data.result.keys():
+    print(data.result.get(freq))
